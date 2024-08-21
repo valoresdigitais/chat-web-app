d31cfe7d07d18589cc32f9c2b39b77d29c9b8d3f
diegodiniz1 <playerdj0@gmail.com>
2024-08-21 04:49:56 -0300

    Remove salva_chave(chave)

--- chat.py
+++ chat.py
@@ -69,7 +69,6 @@ def tab_configuracoes(tab):
     chave = tab.text_input('Adicione sua api key', value=st.session_state['api_key'], type="password")
     if chave != st.session_state['api_key']:
         st.session_state['api_key'] = chave
-        salva_chave(chave)
         tab.success('Chave salva com sucesso')
     
     with tab.popover("⚙️ Temperatura"):
